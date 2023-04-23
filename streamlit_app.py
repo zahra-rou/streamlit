52889971055a3011b617d61849965fa21fc81971
zahra-rou <39680406+zahra-rou@users.noreply.github.com>
2023-04-23 21:01:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.text("🥣 Omega 3 & Bluberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
    
