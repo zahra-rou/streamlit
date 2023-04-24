ea799a326f0166e47e6be508dd0cded324b63b0b
zahra-rou <39680406+zahra-rou@users.noreply.github.com>
2023-04-24 11:54:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,13 @@ my_cur = my_cnx.cursor()
 # streamlit.text(my_data_row)
 
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+# my_data_row = my_cur.fetchone()
 # streamlit.text("The fruit load list contains:")
 # streamlit.text(my_data_row)
 
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
 
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
