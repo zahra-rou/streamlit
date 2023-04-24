f8728942f948c35012937f9d296936560cae5d3f
zahra-rou <39680406+zahra-rou@users.noreply.github.com>
2023-04-24 12:03:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 import streamlit
 import requests
 import pandas
-
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title("My Parents New Healthy Diner")
@@ -29,6 +29,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
    
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
